36059c9ecbb40d5a9f668295e038bd7fca3565d9
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-11 16:38:02 +0100

    Added ui text api.

--- api/accurator.pl
+++ api/accurator.pl
@@ -5,6 +5,8 @@
 
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_server_files)).
+:- use_module(library(http/http_json)).
+:- use_module(library(http/http_parameters)).
 
 http:location(html, cliopatria(html), []).
 http:location(img, cliopatria(img), []).
@@ -13,3 +15,30 @@ user:file_search_path(img, web(img)).
 
 :- http_handler(cliopatria('.'), serve_files_in_directory(html), [prefix]).
 :- http_handler(img('.'), serve_files_in_directory(img), [prefix]).
+:- http_handler(cliopatria(ui_text), ui_text_api,  []).
+
+%%	ui_text_api(+Request)
+%
+%	Retrieves ui text elements, according to the given locale and ui
+%	screen. First it gets the url parameters, second it queries for the
+%	results, after which the data is outputted
+%	as json.
+ui_text_api(Request) :-
+    get_parameters(Request, Options),
+	option(locale(Locale), Options),
+	ResultDic = json{locale:Locale},
+    reply_json_dict(ResultDic).
+
+%%	get_parameters(+Request, -Options)
+%
+%	Retrieves an option list of parameters from the url.
+get_parameters(Request, Options) :-
+    http_parameters(Request,
+        [ui(UI,
+			[description('The ui screen for which text elements are retrieved'),
+			 optional(false)]),
+		 locale(Locale,
+				[description('Locale of language elements to retrieve'),
+				 optional(false)])
+	]),
+    Options = [ui(UI), locale(Locale)].
