4b534ae4bfd0419b4b973a68e6512788cc2ed6f4
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-12 11:08:53 +0100

    Intro screen now gets text from server.

--- web/js/accurator_intro.js
+++ web/js/accurator_intro.js
@@ -1,36 +1,51 @@
 /* Accurator Intro
 */
 
+var txtSlogan, txtSubSlogan, 
+	btnRegister, btnLogin,
+	mdlTxtTitle, mdlFrmUsername, mdlFrmPassword,
+	mdlBtnLogin;
+	
 function introInit() {
-	slogan = "Accurate Art Annotations";
-	subSlogan = "Help us add information to the	artworks depicting birds";
-	register = "Register";
-	login = "or Log in";
-	mdlTitle = "Log in";
-	mdlBtnLogin = "Log in";
-	mdlUsername = "Username";
-	mdlPassword = "Password";
-	registerButtonEvent();
-	addTextSlogan(slogan, subSlogan);
-	addTextButton(register, login);
-	addTextModal(mdlTitle, mdlBtnLogin, mdlUsername, mdlPassword);
+	locale = "en";
+	ui = "http://semanticweb.cs.vu.nl/accurator/ui/bird#intro";
+	$.getJSON("ui_text", {locale:locale, ui:ui})
+		.done(function(data){
+			registerButtonEvent();
+			initVariables(data);
+			addTextSlogan();
+			addTextButton();
+			addTextModal();})
+		.fail(function(data, textStatus){
+			$("#txtSubSlogan").replaceWith('Problem connecting to server, please contact the system administrator');});
 }
 
-function addTextSlogan(slogan, subSlogan) {
-	$("#txtSlogan").prepend(slogan);
-	$("#txtSubSlogan").prepend(subSlogan);
+function initVariables(data) {
+	txtSlogan = data.txtSlogan;
+	txtSubSlogan = data.txtSubSlogan;
+	btnRegister = data.btnRegister;
+	btnLogin = data.btnLogin;
+	mdlTxtTitle = data.mdlTxtTitle;
+	mdlBtnLogin = data.mdlBtnLogin;
+	mdlFrmUsername = data.mdlFrmUsername;
+	mdlFrmPassword = data.mdlFrmPassword;
 }
 
-function addTextButton(register, login) {
-	$("#btnRegister").append(register);
-	$("#btnLogin").append(login);
+function addTextSlogan() {
+	$("#txtSlogan").prepend(txtSlogan);
+	$("#txtSubSlogan").prepend(txtSubSlogan);
 }
 
-function addTextModal(mdlTitle, mdlBtnLogin, mdlUsername, mdlPassword) {
-	$("#mdlTitle").append(mdlTitle);
+function addTextButton() {
+	$("#btnRegister").append(btnRegister);
+	$("#btnLogin").append(btnLogin);
+}
+
+function addTextModal() {
+	$("#mdlTxtTitle").append(mdlTxtTitle);
 	$("#mdlBtnLogin").append(mdlBtnLogin);
-	$("#mdlUsername").append(mdlUsername);
-	$("#mdlPassword").append(mdlPassword);
+	$("#mdlFrmUsername").append(mdlFrmUsername);
+	$("#mdlFrmPassword").append(mdlFrmPassword);
 }
 
 function registerButtonEvent() {
