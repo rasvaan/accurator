7f507c3502fb9b102e98d32a6b6a485ce1c867cc
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-12 16:07:03 +0100

    Made intro javascript more compact.

--- web/js/accurator_intro.js
+++ web/js/accurator_intro.js
@@ -1,51 +1,30 @@
 /* Accurator Intro
 */
 
-var txtSlogan, txtSubSlogan, 
-	btnRegister, btnLogin,
-	mdlTxtTitle, mdlFrmUsername, mdlFrmPassword,
-	mdlBtnLogin;
-	
 function introInit() {
 	locale = "en";
 	ui = "http://semanticweb.cs.vu.nl/accurator/ui/bird#intro";
 	$.getJSON("ui_text", {locale:locale, ui:ui})
 		.done(function(data){
 			registerButtonEvent();
-			initVariables(data);
-			addTextSlogan();
-			addTextButton();
-			addTextModal();})
+			initLabels(data);})
 		.fail(function(data, textStatus){
 			$("#txtSubSlogan").replaceWith('Problem connecting to server, please contact the system administrator');});
 }
 
-function initVariables(data) {
-	txtSlogan = data.txtSlogan;
-	txtSubSlogan = data.txtSubSlogan;
-	btnRegister = data.btnRegister;
-	btnLogin = data.btnLogin;
-	mdlTxtTitle = data.mdlTxtTitle;
-	mdlBtnLogin = data.mdlBtnLogin;
-	mdlFrmUsername = data.mdlFrmUsername;
-	mdlFrmPassword = data.mdlFrmPassword;
+function initLabels(data) {
+	$("#txtSlogan").prepend(data.txtSlogan);
+	$("#txtSubSlogan").prepend(data.txtSubSlogan);
+	$("#btnRegister").append(data.btnRegister);
+	$("#btnLogin").append(data.btnLogin);
+	addTextModal(data);
 }
 
-function addTextSlogan() {
-	$("#txtSlogan").prepend(txtSlogan);
-	$("#txtSubSlogan").prepend(txtSubSlogan);
-}
-
-function addTextButton() {
-	$("#btnRegister").append(btnRegister);
-	$("#btnLogin").append(btnLogin);
-}
-
-function addTextModal() {
-	$("#mdlTxtTitle").append(mdlTxtTitle);
-	$("#mdlBtnLogin").append(mdlBtnLogin);
-	$("#mdlFrmUsername").append(mdlFrmUsername);
-	$("#mdlFrmPassword").append(mdlFrmPassword);
+function addTextModal(data) {
+	$("#mdlTxtTitle").append(data.mdlTxtTitle);
+	$("#mdlBtnLogin").append(data.mdlBtnLogin);
+	$("#mdlFrmUsername").append(data.mdlFrmUsername);
+	$("#mdlFrmPassword").append(data.mdlFrmPassword);
 }
 
 function registerButtonEvent() {
