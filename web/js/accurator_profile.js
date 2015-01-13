0be436850c2a1825aa88a58486e63bc98dac5d61
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-13 15:06:56 +0100

    Added first elements to profile page.

--- web/js/accurator_profile.js
+++ web/js/accurator_profile.js
@@ -1,6 +1,28 @@
 /* Accurator Profile
 */
+var user = "rasvaan";
+var locale = "en";
+var ui = "http://semanticweb.cs.vu.nl/accurator/ui/bird#profile";
+	
+function profileInit() {
+	$.getJSON("ui_elements", {locale:locale, ui:ui, type:"labels"})
+		.done(function(data){
+			registerButtonEvent();
+			initLabels(data);})
+		.fail(function(data, textStatus){
+			$("#txtSubSlogan").replaceWith('Problem connecting to server, please contact the system administrator');});
+}
 
-function profile_init() {
-	alert('Accurator Profile');
+function initLabels(data) {
+	$("#txtSlogan").prepend(data.txtSlogan + " " + user);
+	$("#txtSubSlogan").prepend(data.txtSubSlogan);
+	$("#btnRecommend").append(data.btnRecommend);
+	$("#btnSearch").append(data.btnSearch);
+	$("#lblLastAnnotated").append(data.lblLastAnnotated);
+}
+
+function registerButtonEvent() {
+	$("#btnRecommend").click(function() {
+		document.location.href="/recommendations.html";
+	});	
 }
