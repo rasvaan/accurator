a0acf88e219fa536e8b3a87b0e11f1bca9e29be7
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-14 21:14:52 +0100

    Added events to register screen.

--- web/js/accurator_register.js
+++ web/js/accurator_register.js
@@ -2,11 +2,13 @@
 */
 var locale = "en";
 var ui = "http://semanticweb.cs.vu.nl/accurator/ui/bird#register";
-	
+var lblRegistrationFailed;
+var lblPasswordsMatchFail;
+
 function registerInit() {
 	$.getJSON("ui_elements", {locale:locale, ui:ui, type:"labels"})
 	.done(function(data){
-		registerButtonEvent();
+		registerEvent();
 		initLabels(data);})
 	.fail(function(data, textStatus){
 		setRegisterFailureText("Problem connecting to server, please contact the system administrator.");});
@@ -17,18 +19,52 @@ function initLabels(data) {
 	$("#frmRealName").append(data.frmRealName);
 	$("#frmUsername").append(data.frmUsername);
 	$("#frmPassword").append(data.frmPassword);
+	$("#frmPasswordRepeat").append(data.frmPasswordRepeat);
 	$("#btnRegister").append(data.btnRegister);
+	lblRegistrationFailed = data.lblRegistrationFailed;
+	lblPasswordsMatchFail = data.lblPasswordsMatchFail;
 }
 
-function registerButtonEvent() {
+function registerEvent() {
 	$("#btnRegister").click(function() {
-		document.location.href="/additional_info.html";
+		register();
 	});	
+	// Register on pressing enter
+	$("#regPasswordRepeat").keypress(function(event) {
+		if (event.which == 13) {
+			register();
+		}
+	});
 }
 
 function setRegisterFailureText(text) {
-	alertWell = $.el.div({'class':'well'},text); 
+	alertWell = $.el.div({'class':'well'}, 
+			$.el.p({'class':'text-danger'}, text)); 
 	// clear the current
 	$("#messageWell").empty();
 	$("#messageWell").append(alertWell);
+}
+
+function register() {
+	var register = false
+	var name = $("#regRealName").val();
+	var user = $("#regUsername").val();
+	var password = $("#regPassword").val();
+	var passwordRepeat = $("#regPasswordRepeat").val();
+	
+	if((name == "") || (user == "") || (password == "") || (passwordRepeat == "")){
+		setRegisterFailureText(lblRegistrationFailed);
+	} else if (password != passwordRepeat){
+		setRegisterFailureText(lblPasswordsMatchFail);
+	} else {
+		register = registerServer(name, user, password);
+	}
+	if(register)
+		document.location.href="/additional_info.html";
+}
+
+function registerServer(name, user, password) {
+	console.log("registering user " + name);
+	//Should defenitely be loggin in to server
+	return true;
 }
