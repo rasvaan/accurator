3ed40728a6a250b46e61392426302be66c706b16
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-14 20:45:19 +0100

    Added more handlers to intro screen.

--- web/js/accurator_intro.js
+++ web/js/accurator_intro.js
@@ -1,12 +1,14 @@
 /* Accurator Intro
 */
-locale = "en";
-ui = "http://semanticweb.cs.vu.nl/accurator/ui/bird#intro";
-	
+var locale = "en";
+var ui = "http://semanticweb.cs.vu.nl/accurator/ui/bird#intro";
+var loginWarning, loginIncomplete;
+
 function introInit() {
 	$.getJSON("ui_elements", {locale:locale, ui:ui, type:"labels"})
 		.done(function(data){
 			registerButtonEvent();
+			loginButtonEvent(data);
 			initLabels(data)})
 		.fail(function(data, textStatus){
 			$("#txtSubSlogan").replaceWith('Problem connecting to server, please contact the system administrator');});
@@ -18,12 +20,47 @@ function registerButtonEvent() {
 	});	
 }
 
+function loginButtonEvent(data) {
+	$("#mdlBtnLogin").click(function() {
+		login();
+	});
+	// Login on pressing enter
+	$("#inputPassword").keypress(function(event) {
+		if (event.which == 13) {
+			login();
+		}});
+}
+
+function login() {
+	var username = $("#inputUsername").val();
+	var password = $("#inputPassword").val();
+	var login = false;
+	
+	if(username == "" || password == "") {
+		$(".modal-body").append($.el.p({'class':'text-danger'}, loginIncomplete));
+	} else {
+		login = loginServer();
+	}
+	if(login) {
+		document.location.href="/profile.html";
+	} else {
+		$(".modal-body").append($.el.p({'class':'text-danger'}, loginWarning));
+	}
+}
+
+function loginServer() {
+	//Should defenitely be loggin in to server
+	return true;
+}
+
 function initLabels(data) {
 	$("#txtSlogan").prepend(data.txtSlogan);
 	$("#txtSubSlogan").prepend(data.txtSubSlogan);
 	$("#btnRegister").append(data.btnRegister);
 	$("#btnLogin").append(data.btnLogin);
 	addTextModal(data);
+	loginWarning = data.loginWarning;
+	loginIncomplete = data.loginIncomplete;
 }
 
 function addTextModal(data) {
