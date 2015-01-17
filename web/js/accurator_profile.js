5e410c66eccf9504707ff890ce412b79b136b0a1
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-17 15:50:55 +0100

    Loaded thumbnails using existing cluster search.

--- web/js/accurator_profile.js
+++ web/js/accurator_profile.js
@@ -4,6 +4,7 @@ var user = "rasvaan";
 var locale;
 var ui = "http://semanticweb.cs.vu.nl/accurator/ui/bird#profile";
 var recentItems;
+var initialClusters, enrichedClusters, clusters;
 
 server = {
 		location: getServerUrl()
@@ -73,86 +74,79 @@ function addButtonEvents() {
 function getRecentlyAnnotated() {
 	$.getJSON("recently_annotated", {user:user})
 	.done(function(data){
-			addItems(data);
+			var numberOfItems = data.uris.length;
+			var items = [];
+
+			if(numberOfItems === 0) {
+				$("#cluster0").append(noResultsHtml());
+			} else {
+				for (var i=0; i<numberOfItems; i++) {
+					var uri = data.uris[i];
+					items[i] = new item(uri);
+				}
+				initialClusters[0] = new cluster([], items);
+				enrichedClusters[0] = new cluster([], 'undefined');
+				addItems(0);
+			}
 		})
 	.fail(function(data, textStatus){});
-}
-
-function addItems(data) {
-	var itemUris = data.uris;
-	// Get item enrichments from server, on success add pagination and thumbnails
-	new Pengine({server: server.location + 'pengine',
-				 application: 'enrichment',
-				 ask: 'maplist(enrich_item,' + Pengine.stringify(itemUris, {string:'atom'}) + ', Items),!',
-				 onsuccess: function () {
-					clusterId = 0;
-					recentItems = processEnrichment(this.data, clusterId);
-					// Clone cluster to enable filtering without losing information.
-					if(recentItems.length==0) {
-						$("#cluster0").append(noResultsHtml());
-					} else {
-						var pages = determineNumberOfPages(recentItems.length);
-						$("#cluster0").append(pagination(pages, clusterId));
-						$("#cluster0").append(thumbnails(recentItems, clusterId));
-					}
-	}});
 }
 
 function noResultsHtml() {
 	return $.el.h5('You should start annotating to see some results!');
 }
 
-function determineNumberOfPages(numberOfItems) {
-	var numberOfPages = 0;
-	var restPages = numberOfItems%displayOptions.numberDisplayedItems;
-	
-	//Determine number of items in pagination
-	if(restPages == 0) {
-		numberOfPages = numberOfItems/displayOptions.numberDisplayedItems;
-	} else {
-		numberOfPages = (numberOfItems-restPages)/displayOptions.numberDisplayedItems+1;
-	}
-	return numberOfPages;
-}
-
-function processEnrichment(data) {
-	var sourceItems = data[0].Items;
-	var numberOfItems = sourceItems.length;
-	var items = [];
-		
-	for (var i=0; i<numberOfItems; i++) {
-		var uri = sourceItems[i].uri;
-		var thumb = sourceItems[i].thumb;
-		var link = sourceItems[i].link;
-		var title = truncate(sourceItems[i].title, 60);
-		items[i] = new item(uri, thumb, link, title);
-	}
-	return items;
-}
-
-function item(uri, thumb, link, title) {
-	this.uri = uri;
-	this.thumb = thumb;
-	this.link = link;
-	this.title = title;
-}
-
-function truncate (string, limit) {
-	var chars;
-	var i;
-	
-	chars = string.split('');
-	if (chars.length > limit) {
-		for (i=chars.length - 1; i>-1; --i) {
-			if (i>limit) {
-				chars.length = i;
-			}
-			else if (' ' === chars[i]) {
-				chars.length = i;
-				break;
-			}
-		}
-		chars.push('...');
-	}
-	return chars.join('');
-}
+//function determineNumberOfPages(numberOfItems) {
+//	var numberOfPages = 0;
+//	var restPages = numberOfItems%displayOptions.numberDisplayedItems;
+//	
+//	//Determine number of items in pagination
+//	if(restPages == 0) {
+//		numberOfPages = numberOfItems/displayOptions.numberDisplayedItems;
+//	} else {
+//		numberOfPages = (numberOfItems-restPages)/displayOptions.numberDisplayedItems+1;
+//	}
+//	return numberOfPages;
+//}
+//
+//function processEnrichment(data) {
+//	var sourceItems = data[0].Items;
+//	var numberOfItems = sourceItems.length;
+//	var items = [];
+//		
+//	for (var i=0; i<numberOfItems; i++) {
+//		var uri = sourceItems[i].uri;
+//		var thumb = sourceItems[i].thumb;
+//		var link = sourceItems[i].link;
+//		var title = truncate(sourceItems[i].title, 60);
+//		items[i] = new item(uri, thumb, link, title);
+//	}
+//	return items;
+//}
+//
+//function item(uri, thumb, link, title) {
+//	this.uri = uri;
+//	this.thumb = thumb;
+//	this.link = link;
+//	this.title = title;
+//}
+//
+//function truncate (string, limit) {
+//	var chars;
+//	var i;
+//	
+//	chars = string.split('');
+//	if (chars.length > limit) {
+//		for (i=chars.length - 1; i>-1; --i) {
+//			if (i>limit) {
+//				chars.length = i;
+//			}
+//			else if (' ' === chars[i]) {
+//				chars.length = i;
+//				break;
+//			}
+//		}
+//		chars.push('...');
+//	}
+//	return chars.join('');
+//}
