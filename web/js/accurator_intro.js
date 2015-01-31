f436a4e926375f0eef51756faa3f7cef42714f48
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-31 22:33:31 +0000

    Moved login modal functionallity to utilities.

--- web/js/accurator_intro.js
+++ web/js/accurator_intro.js
@@ -9,53 +9,24 @@ function introInit() {
 	
 	$.getJSON("ui_elements", {locale:locale, ui:ui, type:"labels"})
 		.done(function(data){
-			registerButtonEvent();
-			loginButtonEvent(data);
+			buttonEvents();
 			initLabels(data);})
 		.fail(function(data, textStatus){
 			$("#txtSubSlogan").replaceWith('Problem connecting to server, please contact the system administrator');});
 }
 
-function registerButtonEvent() {
+function buttonEvents() {
 	$("#btnRegister").click(function() {
 		document.location.href="register.html";
-	});	
-}
-
-function loginButtonEvent(data) {
-	$("#mdlBtnLogin").click(function() {
-		login();
 	});
-	// Login on pressing enter
-	$("#inputPassword").keypress(function(event) {
-		if (event.which == 13) {
-			login();
-		}});
-}
-
-function login() {
-	var user = getUserUriBase() + $("#inputUsername").val();
-	var password = $("#inputPassword").val();
-	
-	if(user == "" || password == "") {
-		$(".modal-body").append($.el.p({'class':'text-danger'}, loginIncomplete));
-	} else {
-		loginServer(user, password);
-	}
-}
-
-function loginServer(user, password) {
-	$.ajax({
-		type: "POST",
-		url: "user/login",
-		data: {"user":user, "password":password},
-		success: function(data, textStatus, request){
-		   if(data.contains("Login failed")) {
-			$(".modal-body").append($.el.p({'class':'text-danger'}, loginWarning));
-		   } else if (data.contains("Login ok")) {
-				document.location.href="additional_info.html";
-		   }
-		}
+	$("#btnLogin").click(function() {
+		onSuccess = function(){
+			document.location.href="profile.html";
+		};
+		onDismissal = function(){
+			$("#modalLogin").modal('hide');
+		};
+		loginModal(onSuccess, onDismissal);
 	});
 }
 
@@ -64,14 +35,6 @@ function initLabels(data) {
 	$("#txtSubSlogan").prepend(data.txtSubSlogan);
 	$("#btnRegister").append(data.btnRegister);
 	$("#btnLogin").append(data.btnLogin);
-	addTextModal(data);
 	loginWarning = data.loginWarning;
 	loginIncomplete = data.loginIncomplete;
-}
-
-function addTextModal(data) {
-	$("#mdlTxtTitle").append(data.mdlTxtTitle);
-	$("#mdlBtnLogin").append(data.mdlBtnLogin);
-	$("#mdlFrmUsername").append(data.mdlFrmUsername);
-	$("#mdlFrmPassword").append(data.mdlFrmPassword);
 }
--- web/js/accurator_utilities.js
+++ web/js/accurator_utilities.js
@@ -29,14 +29,64 @@ function userLoggedIn(initFunction) {
 	.fail(function(){loginModal()});
 }
 
-function loginModal() {
-	$.getJSON("ui_elements", {locale:getLocale(), ui:"http://semanticweb.cs.vu.nl/accurator/ui/bird#login_modal", type:"labels"})
+function loginModal(onSuccess, onDismissal) {
+	var ui = "http://semanticweb.cs.vu.nl/accurator/ui/bird#login_modal";
+	$.getJSON("ui_elements", {locale:getLocale(), ui:ui, type:"labels"})
 	.done(function(data){
-		  $("#mdlTxtTitle").append(data.mdlTxtTitle);
-		  $("#mdlBtnLogin").append(data.mdlBtnLogin);
-		  $("#mdlFrmUsername").append(data.mdlFrmUsername);
-		  $("#mdlFrmPassword").append(data.mdlFrmPassword);
-		  $('#modalLogin').modal();});
+		  loginButtonEvent(onSuccess, onDismissal);
+		  initModalLabels(data);
+		  $("#modalLogin").modal();});
+}
+
+function initModalLabels(data) {
+	$("#mdlTxtTitle").html(data.mdlTxtTitle);
+	$("#mdlBtnLogin").html(data.mdlBtnLogin);
+	$("#mdlFrmUsername").html(data.mdlFrmUsername);
+	$("#mdlFrmPassword").html(data.mdlFrmPassword);
+}
+
+function loginButtonEvent(onSuccess) {
+	$("#mdlBtnLogin").click(function() {
+		login(onSuccess);
+	});
+	// Login on pressing enter
+	$("#inputPassword").keypress(function(event) {
+		if (event.which == 13)
+			login(onSuccess);
+	});
+	$("#modalLogin").on('hidden.bs.modal', function (e) {
+		onDismissal();
+	})
+	$("#mdlBtnClose").click(function() {
+		onDismissal();
+	});
+}
+
+
+function login(onSuccess) {
+	var user = getUserUriBase() + $("#inputUsername").val();
+	var password = $("#inputPassword").val();
+	
+	if(user == "" || password == "") {
+		$(".modal-body").append($.el.p({'class':'text-danger'}, loginIncomplete));
+	} else {
+		loginServer(user, password, onSuccess);
+	}
+}
+
+function loginServer(user, password, onSuccess) {
+	$.ajax({
+		   type: "POST",
+		   url: "user/login",
+		   data: {"user":user, "password":password},
+		   success: function(data, textStatus, request){
+		   if(data.contains("Login failed")) {
+		   $(".modal-body").append($.el.p({'class':'text-danger'}, loginWarning));
+		   } else if (data.contains("Login ok")) {
+		   onSuccess();
+		   }
+		   }
+		   });
 }
 
 function getUserUriBase() {
