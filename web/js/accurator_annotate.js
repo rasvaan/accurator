7501b9219bbb18e50d6e4984aee425e89d78ecb2
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2015-11-10 16:19:10 +0100

    populated the page type field with a static list of options resolves #131

--- web/js/accurator_annotate.js
+++ web/js/accurator_annotate.js
@@ -219,15 +219,12 @@ var substringMatcher = function(strs) {
 	};
 };
 
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-	'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-	'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-	'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-	'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-	'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-	'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-	'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-	'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+var pageType = ['Full page illustration',
+	'Page that contains part of an illustration that is printed on more than one page',
+	'Page that contains both text and small illustrations',
+	'Page that contains both text and illuminated capitals',
+	'Page that contains exclusively text',
+	'Page that contains neither illustrations nor text'
 ];
 
 $('#annotations .typeahead').typeahead({
@@ -236,8 +233,8 @@ $('#annotations .typeahead').typeahead({
 	minLength: 1
 },
 {
-	name: 'states',
-	source: substringMatcher(states)
+	name: 'pageType',
+	source: substringMatcher(pageType)
 });
 
 function maybeRunExperiment() {
