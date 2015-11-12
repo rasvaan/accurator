74e7aa80c61355598d0ac33f3596f8916d3c6584
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-11-12 10:13:46 +0100

    Typeahead added with page types.

--- web/js/accurator_annotate.js
+++ web/js/accurator_annotate.js
@@ -151,7 +151,7 @@ function annotationFields() {
 	 	 locale:locale})
 	.done(function(alternatives){
 		// Add typeahead
-		addTypeAhead(id);
+		addTypeAhead(id, alternatives);
 	});
 }
 
@@ -166,17 +166,27 @@ function annotationField(id, label) {
 	);
 }
 
-function addTypeAhead(id){
+function addTypeAhead(id, alternatives){
+	var array = getAlternativeArray(alternatives.results);
+	console.log(array);
 	$('#annotateInp' + id).typeahead({
 		hint: true,
 		highlight: true,
 		minLength: 1
 	},
 	{
-		name: 'states',
-		source: substringMatcher(states)
+		name: 'alternatives',
+		source: substringMatcher(array)
 	});
 }
+
+function getAlternativeArray(alternatives) {
+	var array = [];
+	for(var i=0; i<alternatives.length; i++)
+		array[i] = alternatives[i].label;
+	return array;
+}
+
 
 var substringMatcher = function(strs) {
 	return function findMatches(q, cb) {
@@ -199,17 +209,6 @@ var substringMatcher = function(strs) {
 		cb(matches);
 	};
 };
-
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
 
 function metadata() {
 	if(displayOptions.showMetadata){
