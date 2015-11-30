054e697cf9c74a032f70e8d15e0f237b6e76ebb5
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-11-30 21:37:37 +0100

    Random results are now viewed as a list.

--- web/js/accurator_results.js
+++ web/js/accurator_results.js
@@ -5,7 +5,7 @@ pagination.js and thumbnail.js
 *******************************************************************************/
 var query = "", locale, experiment, ui, userName, realName;
 var resultsTxtRecommendationsFor, resultsHdrFirst, resultsTxtFirst;
-var clusters = [];
+var clusters = [], list = [];
 
 // Display options deciding how to results get rendered
 display = {
@@ -91,6 +91,9 @@ function events() {
 	});
 }
 
+/*******************************************************************************
+Search, Recommend or Random results
+*******************************************************************************/
 function results(query, userQuery, target) {
 	// Determine whether to recommend or give random results and set layout
 	var recommend = recommenderExperiment();
@@ -98,10 +101,10 @@ function results(query, userQuery, target) {
 	if(query) {
 		search(query);
 	} else if(recommend) {
-		recommendItems(userQuery, target);
+		recommend(userQuery, target);
 		query = "expertise values";
 	} else {
-		randomItems(target);
+		random(target);
 		query = "random";
 	}
 	localStorage.setItem("query", query);
@@ -131,21 +134,7 @@ function search(query, target) {
 	});
 }
 
-function populateResults(query) {
-	// Clear results div
-	$("#resultsDiv").children().remove();
-	// Results layout is either cluster or list
-	if(display.layout === "cluster") {
-		populateClusters(query);
-	} else if(display.layout === "list") {
-		//TODO: flatten results clusters
-		populateList();
-	}
-	// Add control buttons
-	controls();
-}
-
-function recommendItems(target) {
+function recommend(target) {
 	console.log("Recommending items");
 
 	$.getJSON("recommendation", {strategy:'expertise',
@@ -166,70 +155,54 @@ function recommendItems(target) {
 	});
 }
 
-function recommendExpertiseList(target) {
-	console.log("Recommending list of items");
+// function recommendExpertiseList(target) {
+// 	console.log("Recommending list of items");
+//
+// 	$.getJSON("recommendation", {strategy:'expertise',
+// 								 number:12,
+// 								 target:target,
+// 							 	 output_format:'list'})
+// 	.done(function(data){
+// 		var numberOfItems = data.length;
+// 		var items = [];
+//
+// 		for (var i=0; i<numberOfItems; i++) {
+// 			var uri = data[i];
+// 			items[i] = new item(uri);
+// 		}
+// 		addItemList(items);
+// 	})
+// 	.fail(function(data, textStatus){
+// 		$("#resultsDiv").children().remove();
+// 		$("#resultsDiv").append(errorHtml(data, textStatus));
+// 		$(document).prop('title', 'Error on ' + query);
+// 	});
+// }
 
-	$.getJSON("recommendation", {strategy:'expertise',
-								 number:12,
-								 target:target,
-							 	 output_format:'list'})
-	.done(function(data){
-		var numberOfItems = data.length;
-		var items = [];
-
-		for (var i=0; i<numberOfItems; i++) {
-			var uri = data[i];
-			items[i] = new item(uri);
-		}
-		addItemList(items);
-	})
-	.fail(function(data, textStatus){
-		$("#resultsDiv").children().remove();
-		$("#resultsDiv").append(errorHtml(data, textStatus));
-		$(document).prop('title', 'Error on ' + query);
-	});
-}
-
-function randomItems(target) {
+function random(target) {
 	console.log("Providing random items");
 
-	// Populate a list of random items
+	// Get a list of random items
 	$.getJSON("recommendation", {strategy:'random',
 								 number:12,
 								 target:target})
 	.done(function(data){
-		var numberOfItems = data.length;
-		var items = [];
-
-		for (var i=0; i<numberOfItems; i++) {
-			var uri = data[i];
-			items[i] = new item(uri);
-		}
-		addItemList(items);
+		// Always populate a list if random
+		list = data;
+		populateList();
 	});
 }
 
-function populateRandom(target, clusterIndex) {
-	$("#resultsDiv").append(clusterContainer(clusterIndex));
-	$.getJSON("recommendation", {strategy:'random',
-								 number:20,
-								 target:target})
-	.done(function(data){
-		var numberOfItems = data.length;
-		var items = [];
-
-		for (var i=0; i<numberOfItems; i++) {
-			var uri = data[i];
-			items[i] = new item(uri);
-		}
-		initialClusters[clusterIndex] = new cluster([], items);
-		enrichedClusters[clusterIndex] = new cluster([], 'undefined');
-		$("#cluster"+clusterIndex).prepend(
-			$.el.h4(
-				$.el.span({'class':'path-label path-literal'},
-					"random objects")));
-		addItems(clusterIndex);
-	});
+function populateResults(query) {
+	// Results layout is either cluster or list
+	if(display.layout === "cluster") {
+		populateClusters(query);
+	} else if(display.layout === "list") {
+		//TODO: flatten results clusters
+		populateList();
+	}
+	// Add control buttons
+	controls();
 }
 
 /*******************************************************************************
@@ -239,6 +212,9 @@ Show the results in clusters
 function populateClusters(query) {
 	//TODO: localize variables
 	var resultsTxtNoResults = "No results found for ";
+
+	// Clear results div
+	$("#resultsDiv").children().remove();
 
 	if(clusters.length == 0){
 		statusMessage(resultsTxtNoResults, query);
@@ -292,7 +268,8 @@ function addItems(clusterId) {
 			contentType: "application/json",
 			data: JSON.stringify(json),
 			success: function(data) {
-				processEnrichment(data, clusters[clusterId].items);
+				// Replace cluster items with enriched items
+				clusters[clusterId].items = processEnrichment(data);
 				var pages = determineNumberOfPages(clusterId);
 				$("#cluster"+clusterId).append(pagination(pages, clusterId));
 				thumbnails(clusterId);
@@ -300,13 +277,18 @@ function addItems(clusterId) {
 	});
 }
 
-function processEnrichment(data, items) {
+function processEnrichment(data) {
+	var enrichedItems = [];
+
 	for(var i=0; i<data.length; i++) {
+		enrichedItems[i] = {};
 		var uri = data[i].uri;
-		items[i].thumb = data[i].thumb;
-		items[i].link = "annotate.html?uri=" + uri;
-		items[i].title = truncate(data[i].title, 60);
+		enrichedItems[i].uri = uri;
+		enrichedItems[i].thumb = data[i].thumb;
+		enrichedItems[i].link = "annotate.html?uri=" + uri;
+		enrichedItems[i].title = truncate(data[i].title, 60);
 	}
+	return enrichedItems;
 }
 
 function determineNumberOfPages (clusterId) {
@@ -328,41 +310,23 @@ List view
 Show the results in one big list
 *******************************************************************************/
 function populateList() {
-	console.log(jsonResults);
+	// Clear results div
+	$("#resultsDiv").children().remove();
+	addItemList();
 }
 
-function addItemList(items) {
-	var itemUris = [];
-	for(var i=0;i<items.length;i++)
-		itemUris[i] = items[i].uri;
-
+function addItemList() {
 	// Get item enrichments from server, on success add pagination and thumbnails
-	var json = {"uris":itemUris};
+	var json = {"uris":list};
 	$.ajax({type: "POST",
 			url: "metadata",
 			contentType: "application/json",
 			data: JSON.stringify(json),
 			success: function(data) {
-				enrichedItems = processListEnrichment(data);
+				var enrichedItems = processEnrichment(data, list);
 				thumbnailList(enrichedItems);
 		   }
 	});
-}
-
-function processListEnrichment(sourceItems) {
-	var numberOfItems = sourceItems.length;
-	var items = [];
-
-	// console.log("display", display.annotateLink);
-	for (var i=0; i<numberOfItems; i++) {
-		var uri = sourceItems[i].uri;
-		var thumb = sourceItems[i].thumb;
-		//var link = "annotate_image.html?uri=" + uri;
-		var link = "annotate.html?uri=" + uri;
- 		var title = truncate(sourceItems[i].title, 60);
-		items[i] = new item(uri, thumb, link, title);
-	}
-	return items;
 }
 
 function thumbnailList(items) {
