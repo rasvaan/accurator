753ef0fb63d0eae87740bfd0ec97ed81bd9073e0
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-12-16 23:34:08 +0100

    Annotation field api code for returning field type specific data.

--- lib/accurator/annotation.pl
+++ lib/accurator/annotation.pl
@@ -8,37 +8,101 @@
 :- use_module(library(semweb/rdf_label)).
 :- use_module(library(semweb/rdfs)).
 
+:- rdf_meta
+	fields(r,-,-,-),
+	label_property_pair(-, r, -, -).
+
 %%	annotation_fields(-Fields, +Options)
 %
 %	Retrieve the definitions of annotation fields from the triple store
+%	and add to dict
 annotation_fields(Fields, Options) :-
-	get_annotation_fields(Fields, Options).
-
-get_annotation_fields(Fields, Options) :-
 	option(annotation_ui(UI), Options),
 	option(locale(Locale), Options),
-	fragment_fields(Locale, UI, FragmentFields),
-	whole_fields(Locale, UI, WholeFields),
+	fields(auis:fragmentFields, Locale, UI, FragmentFields),
+	fields(auis:wholeFields, Locale, UI, WholeFields),
 	Fields = fields{fragment_fields:FragmentFields, whole_fields: WholeFields}.
 
-fragment_fields(Locale, UI, FragmentFields) :-
-	rdf_has(UI, auis:fragmentFields, RdfList), !,
+%%	fields(+Type, +Locale, +UI, -Fields)
+%
+%	Retrieve ordered list of fields for describing fragments
+fields(Type, Locale, UI, Fields) :-
+	rdf_has(UI, Type, RdfList), !,
 	rdfs_list_to_prolog_list(RdfList, FieldUris),
-	maplist(get_annotation_field(Locale), FieldUris, FragmentFields).
-fragment_fields(_Locale, _UI, []).
+	maplist(get_field(Locale), FieldUris, Fields).
+fields(_Type, _Locale, _UI, []).
 
-whole_fields(Locale, UI, WholeFields) :-
-	rdf_has(UI, auis:wholeFields, RdfList),
-	rdfs_list_to_prolog_list(RdfList, FieldUris),
-	maplist(get_annotation_field(Locale), FieldUris, WholeFields).
-whole_fields(_Locale, _UI, []).
+%%	get_field(+Locale, +UI, -Fields)
+%
+%	Determine type of field and get the appropriate dict
+get_field(Locale, Uri, FieldDict) :-
+	rdf(Uri, rdf:type, FieldTypeUri),
+	iri_xml_namespace(FieldTypeUri, _, FieldType),
+	field(FieldType, Uri, Locale, FieldDict).
 
-get_annotation_field(Locale, Uri, Field) :-
-	%currently only handling literals (yeah, that should be better, fix the info needed about the field)
-	findall(Property-Value,
-			rdf(Uri, Property, literal(lang(Locale, Value))),
-			Properties),
+%%	field(+Type, +Uri, +Locale, -FieldDict)
+%
+%	Get info according to the specified type of field
+field('DropdownField', Uri, Locale, Field) :-
+	field_description(Uri, Locale, Properties),
+	get_source(Uri, Locale, Source), !,
+	dict_pairs(Field, elements, ['source'-Source|Properties]).
+field('SelectField', Uri, Locale, Field) :-
+	field_description(Uri, Locale, Properties),	!,
 	dict_pairs(Field, elements, Properties).
+field('TextField', Uri, Locale, Field) :-
+	field_description(Uri, Locale, Properties),	!,
+	dict_pairs(Field, elements, Properties).
+field('RadioButtonField', Uri, Locale, Field) :-
+	field_description(Uri, Locale, Properties),
+	get_source(Uri, Locale, Source), !,
+	dict_pairs(Field, elements, ['source'-Source|Properties]).
+field('CheckboxField', Uri, Locale, Field) :-
+	field_description(Uri, Locale, Properties),
+	get_source(Uri, Locale, Source), !,
+	dict_pairs(Field, elements, ['source'-Source|Properties]).
+field(_, Uri, Locale, Field) :-
+	field_description(Uri, Locale, Properties),
+	dict_pairs(Field, elements, Properties).
+
+%%	field_description(+Uri, +Locale, -DescriptionList)
+%
+%	Get the label and comment textually describing the field
+field_description(Uri, Locale, [LabelPair, CommentPair]) :-
+	label_property_pair(Uri, rdfs:label, Locale, LabelPair),
+	label_property_pair(Uri, dcterms:comment, Locale, CommentPair).
+
+%%	label_property_pair(+Uri, +Property, +Locale, -PropertyValuePair)
+%
+%	Get the text preferrably in the preferred language and shorten the
+%	property label
+label_property_pair(Uri, Property, Locale, UriLabel-Value) :-
+	get_literal(Uri, Property, Locale, Value),
+	iri_xml_namespace(Property, _, UriLabel).
+
+get_literal(Subject, Predicate, Locale, Literal) :-
+	rdf(Subject, Predicate, literal(lang(Locale, Literal))), !.
+get_literal(Subject, Predicate, _Locale, Literal) :-
+	rdf(Subject, Predicate, literal(lang(en, Literal))), !.
+get_literal(Subject, Predicate, _Locale, Literal) :-
+	rdf(Subject, Predicate, literal(lang(_L, Literal))), !.
+get_literal(Subject, Predicate, _Locale, Literal) :-
+	rdf(Subject, Predicate, literal(Literal)), !.
+
+%%	get_source(+Uri, +Locale, -Source)
+%
+%	Get the source, either in the form of a list of alternatives
+%	extracted according to specified language, or literal defining the
+%	autocompletion parameters.
+get_source(Uri, Locale, Source) :-
+	rdf_has(Uri, auis:source, RdfList),
+	rdfs_list_to_prolog_list(RdfList, SourceList),
+	maplist(extract_literal(Locale), SourceList, Source), !.
+get_source(Uri, _Locale, Source) :-
+	rdf_has(Uri, auis:source, literal(Source)), !.
+
+extract_literal(Locale, literal(lang(Locale, Literal)), Literal) :- !.
+extract_literal(_Locale, literal(lang(en, Literal)), Literal) :- !.
 
 %%	annotations(+Type, +Uri, -Metadata)
 %
