fb400a5006772f95ee798e7a5a4ed51584c04c36
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-02-29 13:48:02 +0100

    Added fashion domain defenition and loaded using void.

--- config-available/accurator.pl
+++ config-available/accurator.pl
@@ -10,5 +10,6 @@
 :- rdf_load_library('accurator-generic').
 :- rdf_load_library('accurator-bird-domain').
 :- rdf_load_library('accurator-bible-domain').
+:- rdf_load_library('accurator-fashion-domain').
 
 :- use_module(api(accurator)).
