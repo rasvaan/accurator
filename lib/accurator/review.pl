51e42f9ec52a2c0a0e9badc317cd9dd59e1365a9
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-03-08 17:11:02 +0100

    Adding of review annotations functional.

--- lib/accurator/review.pl
+++ lib/accurator/review.pl
@@ -1,9 +1,11 @@
 :- module(review, [
 			  review/3,
-			  reviews/3]).
+			  reviews/3,
+			  agreeable_annotations/0]).
 
 :- use_module(library(oa_annotation)).
 :- use_module(library(semweb/rdf_db)).
+:- use_module(library(accurator/annotation)).
 
 :- rdf_meta
 	review(r, -, r).
@@ -24,11 +26,23 @@ reviews(Judgement, User, Uris) :-
 %	Add a review annotation with a judgement by a specified user.
 review(Judgement, User, Uri) :-
 	member(Judgement, [agree,disagree,unsure]), !,% verify value
+	atom_string(Judgement, JudgementString),
+	atom_string(Uri, UriString),
 	Options = [
-		target(Uri),
-		label(literal(Judgement)),
+		target([_{'@id':UriString}]), % we don't do simple..
+		label(Judgement),
+		body(_{'@value':JudgementString}),
 		user(User),
 		motivation('http://www.w3.org/ns/oa#moderating')
 	],
-	debug(review, 'Add review: ~p', [Options]).
-	%rdf_add_annotation(Options, _Annotation).
+	debug(review, 'Add review: ~p', [Options]),
+    rdf_add_annotation(Options, _Annotation).
+
+
+%%	agreeable_annotations
+%
+%	Agree on an agreeable set of annotations
+agreeable_annotations :-
+	% get annotations ubvu based on conceptscheme
+	 annotations(concept_scheme, 'http://purl.org/vocab/nl/ubvu/BiblePageConceptScheme', Annotations),
+	 reviews(agree,'http://accurator.nl/user#rasvaan', Annotations).
