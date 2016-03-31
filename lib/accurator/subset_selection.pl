07dea841c39b7bfe1ea6aae04739d559f5c912fe
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-03-31 22:00:32 +0200

    Added predicate targetting ubvu illustrated pages.

--- lib/accurator/subset_selection.pl
+++ lib/accurator/subset_selection.pl
@@ -3,6 +3,7 @@
 							 text_contains_label/5,
 							 target_graph/3,
 							 target_annotation/3,
+							 target_ubvu_pages/2,
 							 target_description_scanner/0,
 							 target_title_scanner/0,
 							 target_bible_pages/0]).
@@ -175,14 +176,32 @@ scan_description(Work, Options0, Label) :-
 	debug(scan_text, '~p present in: ~p', [LabelLower, DescriptionLower]).
 scan_description(_, _, _).
 
+
+
 %%	target_annotation(+Annotation, +TargetType, +Campaign)
 %
 %	Targets edm works which have the specified annotation
-% target_annotation('http://purl.org/vocab/nl/ubvu/MultipleIllustrationsPage','http://accurator.nl/bible#Target','http://accurator.nl/bible#Campaign').
-target_annotation(Annotation, TargetType, Campaign) :-
+% target_ubvu_pages('http://accurator.nl/ubvu#Target','http://accurator.nl/ubvu#Campaign').
+target_ubvu_pages(TargetType, Campaign) :-
+	illustrated_pages(PageAnnotations),
+	maplist(target_annotation(TargetType, Campaign), PageAnnotations).
+
+illustrated_pages([
+	   'http://purl.org/vocab/nl/ubvu/FullPageIllustration',
+	   'http://purl.org/vocab/nl/ubvu/IllustratedPage',
+	   'http://purl.org/vocab/nl/ubvu/MultipleIllustrationsPage',
+	   'http://purl.org/vocab/nl/ubvu/PartialIllustrationPage',
+	   'http://purl.org/vocab/nl/ubvu/TextAndIllustrationPage'
+]).
+
+%%	target_annotation(+Annotation, +TargetType, +Campaign)
+%
+%	Targets edm works which have the specified annotation
+% target_annotation('http://accurator.nl/bible#Target','http://accurator.nl/bible#Campaign','http://purl.org/vocab/nl/ubvu/MultipleIllustrationsPage').
+target_annotation(TargetType, Campaign, Annotation) :-
 	Options = [target_type(TargetType), campaign(Campaign),
 			  targetter('http://accurator.nl/user#AnnotationScanner')],
-	%find all works with class or sublcass of specified class
+	%find all works with annotatoin
 	findall(Object,
 			(	rdf(AnnotationUri, oa:hasBody, Annotation),
 				rdf(AnnotationUri, oa:hasTarget, Object),
