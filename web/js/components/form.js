1f2c585b7b57c18b05c6b97f5f1781290fdf2219
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-04-02 22:01:21 +0200

    Added questions to form, at least as an array.

--- web/js/components/form.js
+++ web/js/components/form.js
@@ -2,14 +2,20 @@
 Form
 Javascript for adding form elements to pages.
 *******************************************************************************/
-function Form() {
+function Form(questions) {
+    this.questions = questions; // array of questions to ask
     this.node = null;
 
-    this.init();
+    this.init(questions);
 }
 
 Form.prototype.init = function() {
     this.node = this.html();
+
+    for (var i=0; i<this.questions.length; i++) {
+        console.log("adding question", i, " with label ", this.questions[i]);
+        if (this.questions[i] == "country") this.addCountryQuestion();
+    }
 }
 
 Form.prototype.html = function() {
@@ -17,4 +23,9 @@ Form.prototype.html = function() {
         $.el.div({'class':'panel-heading'}),
         $.el.div({'class':'panel-body'})
     ]);
+}
+
+Form.prototype.addCountryQuestion = function() {
+    console.log("should be adding country question");
+    $(this.node).find(".panel-body").append("country");
 }
