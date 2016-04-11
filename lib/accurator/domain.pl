fa6705a319a6a6198f48ad5470c38c18ca28dac0
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-04-11 14:06:31 +0200

    Extended domain api to include possible list of topics.

--- lib/accurator/domain.pl
+++ lib/accurator/domain.pl
@@ -1,8 +1,10 @@
-:- module(domain, [get_domain_settings/2]).
+:- module(domain, [get_domain_settings/2,
+				   get_topic_settings/2]).
 
 /** <module> Domain
 */
 :- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdfs)).
 
 %%	get_domain_settings(-Dic, +Options)
 %
@@ -40,6 +42,8 @@ get_domain_dic(DomainUri, Domain, Dic) :-
 	rdf(DomainUri, accu:hasDescriptiveImage, Image),
 	rdf(Image, accu:hasFilePath, literal(ImagePath)),
 	rdf(Image, accu:brightness, literal(Brightness)),
+	rdf_has(DomainUri, accu:topics, RdfList), !,
+	rdfs_list_to_prolog_list(RdfList, Topics),
 	Dic = domain{domain:Domain,
 				 target:Target,
 				 taxonomy:Taxonomy,
@@ -49,4 +53,21 @@ get_domain_dic(DomainUri, Domain, Dic) :-
 				 ui:UI,
 				 annotation_ui:AnnotationUI,
 				 image:ImagePath,
-				 image_brightness:Brightness}.
+				 image_brightness:Brightness,
+				 topics:Topics}.
+
+
+%%	get_topic_settings(-Dic, +Options)
+%
+%	If no topic is provided, return available topics for . If domain is
+%	the given option and it exists, get dict with values,
+%	otherwise return dic with the generic settings
+get_topic_settings(Dic, Options) :-
+	option(domain(Domain), Options),
+	var(Domain), !,
+	rdf(DomainUri, rdf:type, accu:'Domain'),
+	rdf(DomainUri, rdfs:label, literal(Domain)),
+	findall(Topic,
+			rdf(DomainUri, accu:topics, Topic),
+			Topics),
+	Dic = Topics.
