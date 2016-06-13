504909790a06d2abba44bfe34a1a8ac115bd23d7
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-06-13 13:04:56 +0200

    Added example object.

--- config-available/accurator.pl
+++ config-available/accurator.pl
@@ -8,6 +8,7 @@
 % Load ui rdf
 :- rdf_attach_library(accurator(rdf)).
 :- rdf_load_library('accurator-generic').
+:- rdf_load_library('accurator-example-object').
 %:- rdf_load_library('accurator-bird-domain').
 %:- rdf_load_library('accurator-bible-domain').
 %:- rdf_load_library('accurator-fashion-domain').
