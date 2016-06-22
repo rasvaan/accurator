e72ee70d7ecef68271276fa43658f5b80a32f967
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-06-22 15:17:37 +0200

    Code for coloring rows.

--- web/js/annotations.js
+++ web/js/annotations.js
@@ -33,9 +33,11 @@ function annotationsInit() {
 }
 
 function addRow(annotation) {
+	var rowId = 'annotationsTr' + generateIdFromUri(annotation.uri);
 
 	$(".annotationsTblAnnotations").append(
 		$.el.tr(
+			{'id':rowId},
 			$.el.td(
 				$.el.a(
 					{'href': "review.html?uri=" + annotation.uri},
@@ -52,6 +54,32 @@ function addRow(annotation) {
 					getUserName(annotation.annotator)
 			))
 	));
+
+	colorRow(rowId, annotation);
+}
+
+function colorRow(rowId, annotation) {
+	// color the row according to the review made
+	if (annotation.reviews.length > 0) {
+		// sort reviews to base color on latest
+		annotation.reviews.sort(function(a, b){
+		    var keyA = new Date(a.time);
+		    var keyB = new Date(b.time);
+
+		    // compare the 2 dates
+		    if(keyA < keyB) return -1;
+		    if(keyA > keyB) return 1;
+		    return 0;
+		});
+
+		if (annotation.reviews[0].judgement === "agree") {
+			$("#" + rowId).attr('class', 'success');
+		} else if (annotation.reviews[0].judgement === "unsure") {
+			$("#" + rowId).attr('class', 'warning');
+		} else if (annotation.reviews[0].judgement === "disagree") {
+			$("#" + rowId).attr('class', 'danger');
+		}
+	}
 }
 
 function getAnnotations(domain) {
